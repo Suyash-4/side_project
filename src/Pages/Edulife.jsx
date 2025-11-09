537a9f93a06b95d8d9d29a3ef6bd218e89f2e4b5
Suyash <105059093+Suyash-4@users.noreply.github.com>
2025-11-09 22:29:06 +0530

    Update README.md

--- src/Pages/Edulife.jsx
+++ src/Pages/Edulife.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 
 const Edulife = () => {
-  const pass = "Shubh@678";
+  const pass = "Ayush@678";
   const [showPassword, setShowPassword] = useState(false); // pass initially won't be visible
   const [hideButton, setHideButton] = useState(false); //button will be initially visible
   function reveal() {
